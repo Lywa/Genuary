1490039fe61c144524dd74e6064be1d4d494d8a9
Carla M <30538139+Lywa@users.noreply.github.com>
2021-01-05 23:31:41 +0100

    Genuary Jan-5  Golf coding

---
+++ Jan-5/src/ofApp.cpp
@@ -0,0 +1,82 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    
+    ofSetBackgroundAuto(false);
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    
+   
+    
+    ofNoFill();
+    ofSetCircleResolution(100);
+    ofSetColor(counter%255, counter%50, counter);
+    ofDrawCircle(ofGetWidth()/2 +sin(ofGetElapsedTimeMillis())*10,ofGetHeight()/2+cos(ofGetElapsedTimeMillis())*10, (20+ counter)*sin(ofGetElapsedTimeMillis()));
+    
+    counter++;
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
